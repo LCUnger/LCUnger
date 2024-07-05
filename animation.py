2e5259b93b31ef35011afa5427dc66a42eea2a7c
LCUnger <l.c.unger@student.tudelft.nl>
2024-07-05 16:34:15 +0200

    animation maker, results format, start of iterative methop

--- animation.py
+++ animation.py
@@ -1,7 +1,40 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
-import imageio
 
-import setup
+data_path = 'results.npy'
 
+data = np.load(data_path)
 
+fig, ax = plt.subplots()
+line, = ax.plot([], [])
+
+def animation_init_temperature():
+    ax.set(title='Temperature distribution', xlabel='x [m]', ylabel='T [K]'
+           , xlim=(0, 0.1), ylim=(273, 373))
+    return line,
+
+def animation_function_temperature(i):
+    x = data[i,0,:]
+    T = data[i,1,:]
+    line.set_data(x, T)
+    ax.set_title(f'Temperature distribution, Time step: {i}')
+    return line,
+
+def animation_init_theta():
+    ax.set(title='Theta distribution', xlabel='x [m]', ylabel='Theta [-]'
+           , xlim=(0, 0.1), ylim=(0, 1))
+    return line,
+
+def animation_function_theta(i):
+    x = data[i,0,:]
+    theta = data[i,2,:]
+    line.set_data(x, theta)
+    ax.set_title(f'Theta distribution, Time step: {i}')
+    return line,
+
+anim_temperature = FuncAnimation(fig, animation_function_temperature, init_func=animation_init_temperature, frames=data.shape[0], interval=50,blit=True)
+anim_temperature.save('temperature_animation.gif', writer='pillow', fps=30)
+
+anim_theta = FuncAnimation(fig, animation_function_theta, init_func=animation_init_theta, frames=data.shape[0], interval=50,blit=True)
+anim_theta.save('theta_animation.gif', writer='pillow', fps=30)
--- main.py
+++ main.py
@@ -9,10 +9,23 @@ lambda_arr = np.ones(setup.N_x) * setup.lambda_s
 theta_arr = np.zeros(setup.N_x)
 CA_arr = np.zeros(setup.N_x)
 
-result = np.array([[[x_arr,T_arr],[x_arr,theta_arr]]])
-
+result = np.array([[x_arr,T_arr,theta_arr]])
 
 for i in range(setup.N_t-1):
-    result = np.append(result,[[[x_arr,T_arr],[x_arr,theta_arr]]],axis=0)
+    result = np.append(result,[[x_arr,T_arr,theta_arr]],axis=0)
 
-np.save('results', result)
+while theta_arr[-1]<1:
+    T_arr = setup.T_initial + (setup.T_boundary - setup.T_initial) * x_arr / setup.length
+    theta_arr = np.array([setup.theta_l_func(T) for T in T_arr])
+    CA_arr = np.array([setup.CA_func(T,theta_l) for T,theta_l in zip(T_arr,theta_arr)])
+    lambda_arr = np.array([setup.lambda_func(theta_l) for theta_l in theta_arr])
+    rho_arr = np.array([setup.rho_func(theta_l) for theta_l in theta_arr])
+    T_arr_new = np.zeros(setup.N_x)
+    for j in range(1,setup.N_x-1):
+        T_arr_new[j] = T_arr[j] + setup.delta_T * (lambda_arr[j] * (T_arr[j+1] - 2 * T_arr[j] + T_arr[j-1]) / setup.delta_x**2 - CA_arr[j] * (T_arr[j] - setup.T_ref)) / (rho_arr[j] * setup.delta_x)
+    T_arr = T_arr_new
+    result = np.append(result,[[x_arr,T_arr,theta_arr]],axis=0)
+
+    
+
+# np.save('results', result)
